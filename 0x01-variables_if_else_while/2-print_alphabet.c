fe6b2c86c45d0e623495d346ffb80b3877cfed7c
aminem27 <amine_abbes@hotmail.com>
2023-02-17 09:51:30 -0800

    task2 first

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch < 'z' ; ch++)
+		putchar(ch);
+
+	return (0);
+}
