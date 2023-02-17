d4743028dc58551e591d68ca6742c8e62142542e
aminem27 <amine_abbes@hotmail.com>
2023-02-17 10:06:26 -0800

    task2 4th

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	char ch = 'a';
 
-	putchar(ch);
-	for (ch = 'a'; ch < 'z' ; ch++)
-		putchar(ch +1);
+	for (ch = 'a'; ch <= 'z' ; ch++)
+		putchar(ch);
+	putchar("\n");
 
 	return (0);
 }
