f0b1bdb9252db4e7bb26752eec665628ae0c3b19
aminem27 <amine_abbes@hotmail.com>
2023-02-21 11:37:43 -0800

    task0 first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *word = "_putchar"
+		for (int i = 0; i <= strlrn(word); i++)
+		{
+			_putchar(word[i]);
+		}
+	return (0);
+}
+
