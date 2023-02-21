2ccf86c5145a8b2b8bc5d5bbcedf3729f0a6d4a0
aminem27 <amine_abbes@hotmail.com>
2023-02-21 14:33:45 -0800

    task2 5th

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,14 +12,14 @@ void print_alphabet_x10(void)
 	char i = 'a';
 	int j = 0;
 
-	while (j < 10)
-	{
-		while (i <= 'z')
+		while (j < 10)
 		{
-			_putchar(i);
-			i++;
-		};
-		j++;
-	}
+			j++;
+			while (i <= 'z')
+			{
+				_putchar(i);
+				i++;
+			}
+		}
 	_putchar('\n');
 }
