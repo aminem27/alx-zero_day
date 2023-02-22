d722c3d68a575b951378b0b470f4dd7b5928b836
aminem27 <amine_abbes@hotmail.com>
2023-02-22 14:23:28 -0800

    task15 third

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,16 +8,19 @@
 
 int main(void)
 {
-	long u1 = 1, u2 = 2, aux, S = 3;
+        int i;
+        long u1 = 1, u2 = 2, aux, S = 3;
 
-	while (S < 4000000)
-	{
-		aux = u2;
-		u2 = u2 + u1;
-		S = S + u2;
-		u1 = aux;
-		printf("u1=%ld u2=%ld s=%ld \n", u1, u2, S);
-	}
-	printf("%ld\n", S);
-	return (0);
+        for (i = 2; i < 50; i++)
+        {
+
+                aux = u2;
+                u2 = u2 + u1;
+                u1 = aux;
+		S = u1 + u2;
+		if (S < 4000000)
+			break;
+        }
+        printf("%ld\n", S);
+        return (0);
 }
