5a6bab56e4af6bbba65f2d30ef1804ed7b1d1ee4
aminem27 <amine_abbes@hotmail.com>
2023-02-22 12:43:12 -0800

    task13 first

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,7 +12,19 @@ void som(void)
 	int val = 0, i;
 
 	for (i = 1; i < 1024; i++)
-		if (i % 3 || i % 5)
-			val= val + i;
-	printf("%d", val);
+		if ((i % 3 == 0) || (i % 5 == 0))
+			val = val + i;
+	printf("%d \n", val);
+}
+
+/**
+ * main - Check description
+ * Description: execute function som
+ * Return: 0
+ */
+
+int main(void)
+{
+	som();
+	return (0);
 }
