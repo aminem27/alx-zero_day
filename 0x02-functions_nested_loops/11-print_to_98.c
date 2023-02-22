121c28f1f3b7d6a42c6e9bea0582699d07434694
aminem27 <amine_abbes@hotmail.com>
2023-02-21 18:44:01 -0800

    task 11 first

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Check description
+ * Description:increment to 98 or decrement
+ * @n :Number
+ * Return: Nothing
+ */
+
+void print_to_98(int n);
+{
+	int val;
+
+	val = n;
+	if (n < 98)
+	{
+		while (val <= 98)
+		{
+			print("%d", val);
+			if (val != 98)
+				print(", ");
+			val++;
+		}
+	}
+	else
+		while (val >= 98)
+		{
+			print("%d", val);
+			if (val != 98)
+				print(", ");
+			val--;
+		}
+}
