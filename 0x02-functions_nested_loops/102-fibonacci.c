e18cc9364544c6227c97b156177924886ffad884
aminem27 <amine_abbes@hotmail.com>
2023-02-22 13:20:46 -0800

    task14 first

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Check description
+ * Description: execute function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, u1 = 1, u2 = 2, aux;
+
+	printf("%d, ", u1);
+	printf("%d", u2);
+	for (i = 2; i < 50; i++)
+	{
+
+		aux = u2;
+		u2 = u2 + u1;
+		u1 = aux;
+		printf(", %d", u2);
+	}
+
+	return (0);
+}
