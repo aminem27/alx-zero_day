9c9c535ea1c5d33dbbcffd15a0d72b722aa914aa
aminem27 <amine_abbes@hotmail.com>
2023-02-21 18:17:22 -0800

    task 10 first

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - Check description
+ * Description: add two number
+ * Return: Number
+ */
+
+int add(int x, int y)
+{
+	int val;
+
+	val = x + y;
+	return(val);
+}
