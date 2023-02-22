7cf56977bf7c62489d8db6b77939b53eb20db15a
aminem27 <amine_abbes@hotmail.com>
2023-02-22 13:47:48 -0800

    task15 first

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Check description
+ * Description: execute function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long u1 = 1, u2 = 2, aux, S = 3;
+
+	for (i = 2; i < 50; i++)
+	{
+
+		aux = u2;
+		u2 = u2 + u1;
+		while (S < 4000000)
+			S = S + u2;
+		u1 = aux;
+		printf("%ld\n", S);
+	}
+	return (0);
+}
