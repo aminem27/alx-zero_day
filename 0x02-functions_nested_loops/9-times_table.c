02735ae0d9ca3fc4828a8c776d05d1dad27547e4
aminem27 <amine_abbes@hotmail.com>
2023-02-21 17:38:40 -0800

    task9 first

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - Check description
+ * Description: print table
+ * Return: Nothing
+ */
+
+void times_table(void);
+{
+	int l ,c ,val;
+	for (c = 0; c < 10; c++)
+	{
+		for (l = 0; l < 10; l++)
+		{
+			val = c * l;
+			_putchar('0' + val);
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+
+}
