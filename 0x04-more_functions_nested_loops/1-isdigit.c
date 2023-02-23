f9c1aec005ee3900b41b0d5b7ddf5adc1b0906df
aminem27 <amine_abbes@hotmail.com>
2023-02-23 13:54:40 -0800

    task1 first

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i = 0;
+
+	if (c <= 57 && c >= 48)
+	{
+		i = 1;
+	}
+	return (i);
+}
