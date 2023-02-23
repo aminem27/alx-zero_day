2be899173ddb2470a936d32a3e81171e6931fa4d
aminem27 <amine_abbes@hotmail.com>
2023-02-23 08:37:15 -0800

    task0 4th

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include "main.h"
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * positive_or_negative - Entry point
+ * @n :Number integer
+ * Return: Nothing(Success)
  */
-int main(void)
+void positive_or_negative(int n)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-	return (0);
 }
