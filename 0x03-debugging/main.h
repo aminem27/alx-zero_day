b1fa8b7fcf34f7be2e5bc88ccf2b06d4bd99b0ae
aminem27 <amine_abbes@hotmail.com>
2023-02-23 11:14:31 -0800

    task 3 4th

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,6 +5,6 @@
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 #endif /* MAIN_H */
