7e0beadb7ffc0c7f8582832a0db891d38ab06304
aminem27 <amine_abbes@hotmail.com>
2023-02-23 18:06:40 -0800

    task11 first

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i, n, val = 612852475143;
+	
+	n = val / 2;
+	for (i = 2; i < n; i++)
+	{
+		if ( val % i == 0 && i != val)
+		{
+			printf("%ld ",i);
+			val = val / i;
+		}
+		if (val % i == 0 && i == val)
+			{
+				printf("%ld", i);
+				break;
+			}
+	}
+	printf("\n");
+
+	return (0);
+}
