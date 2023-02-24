2b461d6eade694a49ecf1466e4fb0473e3b471a2
aminem27 <amine_abbes@hotmail.com>
2023-02-23 16:39:15 -0800

    task9 second

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 /**
- * main :fizz buzz sequence
- * return always 0
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 1;i < 100; i++)
+	for (i = 1; i < 100; i++)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else
 			if (i % 3 == 0)
 				printf("Fizz ");
 			else
-				if(i % 5 == 0)
+				if (i % 5 == 0)
 					printf("Buzz ");
 				else
-					printf("%d ",i);
+					printf("%d ", i);
 	printf("Buzz\n");
-	return(0);
+	return (0);
 }
