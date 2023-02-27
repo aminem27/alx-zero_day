19db06de248732d709e95bc25a496214232e2ff6
aminem27 <amine_abbes@hotmail.com>
2023-02-27 15:57:42 -0800

    task 4 first

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * print_rev -  a function .
+ * @s: an inout character pointer
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len != 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+}
