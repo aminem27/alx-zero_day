55d2bee3e94b4e76814176ff884b109481b94453
aminem27 <amine_abbes@hotmail.com>
2023-02-27 15:29:59 -0800

    task2 first

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strlen -  a function that swaps the values of two integers.
+ * @s: an inout character pointer
+ * Return: Number
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = strlen(*s);
+
+	return (n);
+}
