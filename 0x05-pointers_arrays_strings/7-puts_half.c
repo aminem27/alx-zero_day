b086e7fd6b0b5edcdd1317246f944275f320fa37
aminem27 <amine_abbes@hotmail.com>
2023-02-27 17:40:33 -0800

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - a function ,
+ * starting with the first character, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len -= 1;
+
+sh: 1: q: not found
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
