b7b3ec59530430978687213646a277dcf12149a9
aminem27 <amine_abbes@hotmail.com>
2023-02-27 17:53:14 -0800

    task7_4

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,9 +12,7 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
-
-	len -= 1;
-	for (i = len / 2 ; i <= len ; i++)
+	for (i = (len - 1) / 2 ; i < len  ; i++)
 	_putchar(str[i]);
 
 	_putchar('\n');
