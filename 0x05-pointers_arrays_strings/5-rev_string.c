55fe7b613483191cd921f07f5099568b855000dd
aminem27 <amine_abbes@hotmail.com>
2023-02-27 16:43:35 -0800

    task 5 second

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,15 @@
 void rev_string(char *s)
 {
 	int i, len = 0;
+	char *rev;
 
 	while (s[len] != '\0')
 		len++;
-	for (i = 0; i >= len / 2; i++)
+	for (i = 0; i < len ; i++)
 	{
-		s[i] = s[len - 1 - i];
+		rev[i] = s[len - 1 - i];
 	}
+	rev[len] = '\0'
+	s = rev;
 	_putchar('\n');
 }
