e83d2ac1c45a27303638d099bb3283efe59c37a9
aminem27 <amine_abbes@hotmail.com>
2023-03-27 17:43:32 -0700

    betty style print.c

---
+++ print.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * _printf - A function that print all its parameters.
+ * @format: number of parameters
+ * @...: Other parameters
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
